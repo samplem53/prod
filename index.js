ca63f39a6f65e13502dc9a896519dcfbd1227424
samplem53 <160483683+samplem53@users.noreply.github.com>
2024-02-19 16:29:16 +0530

    Update index.js
    serverless connection

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv/config');
 const mongoose = require('mongoose');
 const app = require('./app')
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log("MongoDB Connection Failed"));
+const connectDB = async () => {
+    mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("MongoDB Connection Failed"));
+}
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
-    // console.log(`App running on port ${port}`);
-});
+connectDB().then(() => {
+    app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+    });
+})
